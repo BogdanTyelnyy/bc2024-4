f4c21fc43b88938856fd69c202e081bd6ecef038
Bogdan Tyelnyy bc2024-4 <bogdan.tlny@gmail.com>
2024-10-20 04:17:38 +0300

    Readlised method PUT, method GET calls a method PUT if there is no serched picture in the cache

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { program } = require('commander');
 const http = require('http');
 const fs = require('node:fs');
-const agent = require('superagent');
+const superagent = require('superagent');
 program
     .option('-h, --host <address>', 'Server address')
     .option('-p, --port <number>', 'Server port')
@@ -22,27 +22,42 @@ if (!opts.host) {
 }
 
 const server = http.createServer((req, res) => {
+    //console.log(req.method + " " + req.url);
+    const pictureUrl = "https://http.cat" + req.url + ".jpg";
+    const pictureFile = opts.cache + req.url + '.jpg';
     if (req.method === "GET") {
-        const picture = opts.cache + req.url + '.jpg';
-        fs.promises.readFile(picture)
+        fs.promises.readFile(pictureFile)
             .then(readPicture => {
                 res.setHeader('Content-Type', 'image/jpeg');
                 res.writeHead(200);
                 res.end(readPicture);
             })
             .catch(error => {
+                superagent
+                    .put(`http://${opts.host}:${opts.port}${req.url}`)
+                    .catch(error => {});
                 res.writeHead(404);
                 res.end('Picture has not found');
             });
     } else if (req.method === "PUT") {
-       
+        superagent.get(pictureUrl)
+            .then(picture => {
+                fs.promises.writeFile(pictureFile, picture.body);
+                res.writeHead(201);
+                res.end();
+            })
+            .catch(error => {
+                res.writeHead(404);
+                res.end();
+            });
+        return;
     } else if (req.method === "DELETE") {
-        const picture = opts.cache + req.url + '.jpg';
-        fs.promises.rm(picture, {
+        fs.promises.rm(pictureFile, {
             force : true
         });
         res.writeHead(200);
         res.end();
+        return;
     } else {
         res.writeHead(405);
         res.end();
